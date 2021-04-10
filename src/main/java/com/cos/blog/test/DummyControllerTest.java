a66119310bff5ad9e0ef160c21aa75991146c0a7
9595kys <9595kys@gmail.com>
2021-04-10 19:59:59 +0900

    chapter26 select(findById())

--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -1,6 +1,10 @@
 package com.cos.blog.test;
 
+import java.util.function.Supplier;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,6 +34,28 @@ public class DummyControllerTest {
 		user.setRole(RoleType.USER);
 		userRepository.save(user);
 		return "회원가입이 완료되었습니다.";
+	}
+	
+	
+	//{id} 주소로 파라미터를 전달받을 수 있음
+	@GetMapping("/dummy/user/{id}")
+	public User detail(@PathVariable int id) { // 주소로 들어온 id를 받음
+//		User user = userRepository.findById(id);
+		 // CurdRepository의 findById() 리턴타입은 Optional임!
+		// optional은 db에서 찾는 값 null이라던가 오류 날 때를 대비해 optional로 가져와 컨버팅 필요
+
+//		User user = userRepository.findById(id).orElseThrow(new Supplier<IllegalArgumentException>() {
+//			@Override
+//			public IllegalArgumentException get() {
+//				return new IllegalArgumentException("해당 유저는 없습니다. id: "+id);
+//			}
+//		});
+				//.get()은 무조건 가져오기(위험)
+		User user = userRepository.findById(id).orElseThrow(()->{
+			return new IllegalArgumentException("해당 사용자는 없습니다. id: " + id);
+		});
+		
+		return user;
 	}
 
 }
